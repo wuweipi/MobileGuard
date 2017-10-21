b74554f45a350b6284054c9b3a017c751e14dc18
wuweipi <996384096@qq.com>
2017-10-21 21:02:26 +0800

    手机防盗密码

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by 天地科技 on 2017/10/16.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public  String apkurl;
+    public String versionCode;
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/utils/MyUtils.java
@@ -0,0 +1,23 @@
+package cn.edu.gdmec.android.mobileguard.m1home.utils;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * Created by 天地科技 on 2017/10/16.
+ */
+
+public class MyUtils {
+    public  static  String getVersion(Context context){
+        PackageManager packageManager = context.getPackageManager();
+        try{
+            PackageInfo packageInfo= packageManager.getPackageInfo(context.getPackageName(),0);
+            return packageInfo.versionName;
+
+        }catch(PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            return"";
+        }
+    }
+}
