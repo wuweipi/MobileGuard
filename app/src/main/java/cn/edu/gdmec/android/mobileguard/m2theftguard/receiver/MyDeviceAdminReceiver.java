e8d40a700404ea7b3bdc436bec3563ba0faa8a94
wuweipi <996384096@qq.com>
2017-10-29 20:25:35 +0800

    指令

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/MyDeviceAdminReceiver.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 天地科技 on 2017/10/29.
+ */
+
+public class MyDeviceAdminReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+    }
+}
+
